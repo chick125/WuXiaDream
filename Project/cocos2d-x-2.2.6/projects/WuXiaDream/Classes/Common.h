ce252e7c5e84073f730e70afa68541e2d8c81f9b
chick125 <chick125@126.com>
2015-03-26 16:19:59 +0800

    修改程序目录结构
    修改程序目录结构

---
+++ Project/cocos2d-x-2.2.6/projects/WuXiaDream/Classes/Common.h
@@ -0,0 +1,16 @@
+//
+//  Common.h
+//  WuXiaDream
+//
+//  Created by 何金 北京 on 15/3/26.
+//
+//
+
+#ifndef WuXiaDream_Common_h
+#define WuXiaDream_Common_h
+
+#include "cococ2d.h"
+#include "cocos-ext.h"
+USING_NS_CC;
+USING_NS_CC_EXT;
+#endif
